e000a5db1d934e6e3c3d704c9f3f6855c0733850
louie <louieweng@outlook.com>
2022-09-30 22:19:49 -0700

    feat: added the data structure that will represent a minesweeper game board

--- phase_1_cli/lib/board.ex
+++ phase_1_cli/lib/board.ex
@@ -9,25 +9,32 @@ defmodule Board do
 
   @x_size 10
   @y_size 8
-  @mine_threshold 30
+  @mine_threshold 33
 
-  @mine "X"
+  @type board_mine :: X | O
+  @type board_row :: %{number => board_mine}
+  @type board :: %{number => board_row}
 
-  @type
-
+  @spec generate_row() :: board_row()
   def generate_row() do
-
+    Enum.reduce(1..@x_size, %{}, fn x, acc ->
+      Map.put(acc, x, randomize_tile())
+    end)
   end
 
+  @spec generate_board() :: board()
   def generate_board() do
-
+    Enum.reduce(1..@y_size, %{}, fn x, acc ->
+      Map.put(acc, x, generate_row())
+    end)
   end
 
+  @spec randomize_tile() :: board
   def randomize_tile() do
-    if :rand.uniform() <= @mine_threshold do
-
+    if :rand.uniform(100) <= @mine_threshold do
+      "X"
     else
-
+      "O"
     end
   end
 
